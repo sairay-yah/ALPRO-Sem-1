f19033b79e6a3f5300533b2725142e3b8297f5ec
Syakira Al-'Aafiyah <alaafiyah12@gmail.com>
2025-04-09 16:16:31 +0700

    Add files via upload
    kalkulator sederhana (penjumlahan, pengurangan, perkalian, pembagian)

---
+++ kalkulator/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Selamat Datang di Program Kalkulator Sederhana...");
+while (true) {
+    Console.Write("Nilai Pertama : ");
+    int nilai1 = int.Parse(Console.ReadLine());
+    Console.Write("Nilai Kedua : ");
+    int nilai2 = int.Parse(Console.ReadLine());
+
+    Console.Write("Pilih Jenis Operasi (+/-/*/:) : ");
+    string operasi = Console.ReadLine();
+    if (operasi == "+") {
+        int hasil = nilai1+nilai2;
+        Console.WriteLine("Hasil Penjumlahan = " + hasil);
+    }
+    else if (operasi == "-") {
+        int hasil = nilai1-nilai2;
+        Console.WriteLine("Hasil Pengurangan = " + hasil);
+    }
+    else if (operasi == "*") {
+        int hasil = nilai1*nilai2;
+        Console.WriteLine("Hasil Perkalian = " + hasil);
+    }
+    else if (operasi == ":") {
+        int hasil = nilai1/nilai2;
+        Console.WriteLine("Hasil Pembagian = " + hasil);
+    }
+    else {
+        Console.WriteLine("Maaf, Operasi Invalid");
+    }
+    Console.Write("Ulangi Program? (y/n) : ");
+    string ulang = Console.ReadLine();
+    if (ulang == "n") {
+        Console.WriteLine("Terimakasih...");
+        return;
+    }
+    else if (ulang == "y"){
+        Console.Write("\n");
+        continue;
+    }
+    else{
+        Console.WriteLine("Maaf, Operasi Invalid, program dihentikan");
+        return;
+    }
+}
